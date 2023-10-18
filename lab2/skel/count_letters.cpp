26b4f518fa883b77d5d0452721a30227d862b04f
Mihnea Branzeu <mihneabranzeu@Mihneas-Laptop.local>
2023-10-18 09:29:51 +0300

    letters

--- lab2/skel/count_letters.cpp
+++ lab2/skel/count_letters.cpp
@@ -11,13 +11,14 @@ using namespace std;
 int main() {
     map<char, int> letters;
     vector<string> words;
+    int i;
 
     for (int i = 32; i < 128; i++) {
         letters[(char)i] = 0;
     }
 
     #pragma omp parallel for private(i) schedule(auto)
-    for (int i = 1; i <= 100; i++) {
+    for (i = 1; i <= 100; i++) {
         string filename = "files/f" + to_string(i);
         char content[10000];
         FILE *file;
@@ -31,7 +32,7 @@ int main() {
     }
 
     #pragma omp parallel for private(i) schedule(auto)
-    for (int i = 0; i < words.size(); i++) {
+    for (i = 0; i < words.size(); i++) {
         for (int j = 0; j < words[i].size(); j++) {
             #pragma omp atomic
             letters[words[i][j]]++;
