4358adec7cfeb03175ccd601330af1a1d1f5631d
Mihnea Branzeu <mihneabranzeu@Mihneas-Laptop.local>
2023-10-18 09:27:26 +0300

    letters

--- lab2/skel/count_letters.cpp
+++ lab2/skel/count_letters.cpp
@@ -16,6 +16,7 @@ int main() {
         letters[(char)i] = 0;
     }
 
+    #pragma omp parallel for private(i) schedule(auto)
     for (int i = 1; i <= 100; i++) {
         string filename = "files/f" + to_string(i);
         char content[10000];
@@ -29,8 +30,10 @@ int main() {
         fclose(file);
     }
 
+    #pragma omp parallel for private(i) schedule(auto)
     for (int i = 0; i < words.size(); i++) {
         for (int j = 0; j < words[i].size(); j++) {
+            #pragma omp atomic
             letters[words[i][j]]++;
         }
     }
