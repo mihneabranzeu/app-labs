614f850c9ee336e3295b24264277b0ecb0a6da76
Mihnea Branzeu <mihneabranzeu@Mihneas-Laptop.local>
2023-10-25 08:51:18 +0300

    lab3

--- lab3/skel/main.c
+++ lab3/skel/main.c
@@ -2,8 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
+#include <sys/time.h>
 
 #define NO_THREADS 4
+#define SECOND_MICROS		1000000.f
+
 
 // scriere in fisier
 void write_file(char *filename, char *type, double time, long sum) {
@@ -16,6 +20,8 @@ int main() {
     FILE *file = fopen("input.txt", "r");
     int size, i;
     int *numbers;
+	struct timeval start1, end1, start2, end2, start3, end3;
+
 
     long sum1 = 0, sum2 = 0, sum3 = 0;
     double t1, t2;
@@ -36,21 +42,79 @@ int main() {
             fclose(file);
         }
 
+        #pragma omp barrier
+
+        #pragma omp master
+        {
+            gettimeofday(&start1, NULL);
+        }
+        
         #pragma omp for reduction(+:sum1) private(i)
         for (i = 0; i < size; i++) {
             sum1 += numbers[i];
         }
 
-        #pragma omp for shared(sum2) private(i)
+        #pragma omp master
+        {
+            gettimeofday(&end1, NULL);
+            t1 = (end1.tv_sec - start1.tv_sec) * SECOND_MICROS + end1.tv_usec - start1.tv_usec;
+            time1 = t1 / SECOND_MICROS;
+        }
+
+        #pragma omp master
+        {
+            gettimeofday(&start2, NULL);
+        }
+
+        #pragma omp for private(i)
         for (i = 0; i < size; i++) {
             #pragma omp atomic
             sum2 += numbers[i];
         }
 
-        #pragma omp for shared(sum3) private(i)
+        #pragma omp master
+        {
+            gettimeofday(&end2, NULL);
+            t2 = (end2.tv_sec - start2.tv_sec) * SECOND_MICROS + end2.tv_usec - start2.tv_usec;
+            time2 = t2 / SECOND_MICROS;
+        }
+
+        #pragma omp master
+        {
+            gettimeofday(&start3, NULL);
+        }
+
+        #pragma omp for private(i)
         for (i = 0; i < size; i++) {
             #pragma omp critical
-            sum2 += numbers[i];
+            sum3 += numbers[i];
+        }
+
+        #pragma omp master
+        {
+            gettimeofday(&end3, NULL);
+            t2 = (end3.tv_sec - start3.tv_sec) * SECOND_MICROS + end3.tv_usec - start3.tv_usec;
+            time3 = t2 / SECOND_MICROS;
+        }
+
+        #pragma omp barrier
+
+        #pragma omp sections
+        {
+            #pragma omp section
+            {
+                write_file("reduction.txt", "atomic", time1, sum1);
+            }
+
+            #pragma omp section
+            {
+                write_file("atomic.txt", "atomic", time2, sum2);
+            }
+
+            #pragma omp section
+            {
+                write_file("critical.txt", "critical", time3, sum3);
+            }
         }
     }
 
@@ -58,9 +122,6 @@ int main() {
     
 
     // de paralelizat in 3 moduri - atomic, critical si reduction, de masurat timpii de executi
-    printf("Sum1: %ld\n", sum1);
-    printf("Sum2: %ld\n", sum2);
-    printf("Sum3: %ld\n", sum3);
 
     // TODO: de scris timpii de executie in 3 fisiere folosind sections (fiecare scriere intr-un section)
 
